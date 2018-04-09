a903eb9cb976855f4528f5f85269bb8723d7ec4e
x00136708 <doranronan14@gmail.com>
2018-04-09 14:15:22 +0100

    Small changes to

--- target/scala-2.12/routes/main/controllers/security/javascript/JavaScriptReverseRoutes.scala
+++ target/scala-2.12/routes/main/controllers/security/javascript/JavaScriptReverseRoutes.scala
@@ -1,12 +1,7 @@
 
 // @GENERATOR:play-routes-compiler
-<<<<<<< HEAD
-// @SOURCE:/home/brandon/Documents/SemesterCA/conf/routes
-// @DATE:Mon Apr 09 01:04:48 IST 2018
-=======
-// @SOURCE:C:/Users/doran/Documents/2ndYearProject/semesterca/conf/routes
-// @DATE:Sun Apr 08 20:03:29 BST 2018
->>>>>>> aa4120e9331252f7dd028526e355ada7dc39cce8
+// @SOURCE:/home/wdd/webapps/semesterca/conf/routes
+// @DATE:Mon Apr 09 14:14:51 IST 2018
 
 import play.api.routing.JavaScriptReverseRoute
 
